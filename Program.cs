7ae1db13588b78863b3bb4915581e5b361d2ce94
svyatoslavPy <svyatoslavik9@gmail.com>
2023-10-27 18:46:33 +0200

    class-work

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!!";
+
+            Console.WriteLine(message);
+        }
+    }
+}
