e4c79f9845e7074397403dbcfeee7e55d7819007
svyatoslavPy <svyatoslavik9@gmail.com>
2023-11-11 15:54:42 +0100

    Циклы

--- Program.cs
+++ Program.cs
@@ -4,196 +4,133 @@ namespace ClassWork
 {
     class Program
     {
-        enum CalcOperations
-        {
-            Plus = '+',
-            Minus = '-',
-            Myltiply = '*',
-            Division = '/'
-        }
-
-        enum CodePizza
-        {
-            Margaritha = 3,
-            Spicy = 2,
-            Mexico = 4
-        }
-        
-        enum PricePizza
-        {
-            Margaritha = 8,
-            Spicy = 9,
-            Mexico = 10
-        }
-
-        enum CodeDrinks
-        {   
-            Cola = 1,
-            Sprite = 2,
-            Fanta = 3
-        }
-
-        enum PriceDrinks
-        {
-            Cola = 2,
-            Sprite = 1,
-            Fanta = 3
-        }
-        
-   
-
         static void Main(string[] args)
         {
             // task 1
             try
             {
-                Console.Write("Введите первое число: ");
-                int firstNum = Convert.ToInt32(Console.ReadLine());
+                int countNumberMultipleOfFive = 0;
+                int start = 0;
+                int end = 0;
 
-                Console.Write("Введите второе число: ");
-                int secondNum = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Выберите операцию: ( +, /, */ - ): ");
-                char operation = Convert.ToChar(Console.ReadLine() ?? string.Empty);
-
-                switch (operation)
+                do
                 {
-                    case (char)CalcOperations.Plus:
-                        Console.WriteLine(firstNum + secondNum);
-                        break;
-                    case (char)CalcOperations.Minus:
-                        Console.WriteLine(firstNum - secondNum);
-                        break;
-                    case (char)CalcOperations.Division:
-                        if (secondNum == 0)
-                        {
-                            throw new DivideByZeroException("You dont divide at zero, please!!!");
-                        }
+                    Console.Write("Enter a start: ");
+                    start = Convert.ToInt32(Console.ReadLine());
 
-                        Console.WriteLine(firstNum / secondNum);
-                        break;
-                    case (char)CalcOperations.Myltiply:
-                        Console.WriteLine(firstNum * secondNum);
-                        break;
-                    default:
-                        throw new Exception("Not correctly this operation");
+                    Console.Write("Enter a end: ");
+                    end = Convert.ToInt32(Console.ReadLine());
+                } while ((start <= 0 || end <= 0));
+
+                Console.WriteLine("Number all in range: ");
+                for (int i = start; i <= end; i++)
+                {
+                    Console.WriteLine(i);
+
+                    if (i % 7 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                    if (i % 5 == 0)
+                    {
+                        countNumberMultipleOfFive++;
+                    }
+                }
+
+                Console.WriteLine($"Count number multiple of five - {countNumberMultipleOfFive}");
+                Console.WriteLine("Number less: ");
+
+                for (int i = end; i >= start; i--)
+                {
+                    Console.Write(i);
                 }
             }
-            catch (DivideByZeroException ex)
+            catch (FormatException)
             {
-                Console.WriteLine(ex);
+                Console.WriteLine("Not correct, again write number");
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex);
+                Console.WriteLine(ex.Message);
             }
 
-            // TASK 2
 
-            Console.WriteLine("Welcome to our pizza cafe! ");
-            Console.Write("Are you need menu for choise pizza ( Yes or No ): ");
-
-            static void PrintMenu()
+            // task 2
+            try
             {
-                Console.WriteLine("Menu");
-                Console.WriteLine("Pizza: ");
-                Console.WriteLine("Pizza Margaritha, code - 3");
-                Console.WriteLine("Pizza Spicy, code - 2");
-                Console.WriteLine("Pizza Mexico, code - 4");
-                Console.WriteLine("Drinks: ");
-                Console.WriteLine("Coca-cola, code - 1");
-                Console.WriteLine("Sprite, code - 2");
-                Console.WriteLine("Fanta, code - 3");
-            }
+                Console.WriteLine();
+                Console.WriteLine("Enter a number: ");
 
-            
-            static void Menu()
-            {
-                try
+                int evenCount = 0;
+                int oddCount = 0;
+                int countNumberMultipleOfNine = 0;
+                int sumEven = 0;
+                int sumOdd = 0;
+                int sumMultipleOfNine = 0;
+
+
+                for (int i = 0; i < 2; i++)
                 {
-                    PrintMenu();
-                    Console.Write("Enter a code for choise pizza: ");
-                    int choisePizza = Convert.ToInt32(Console.ReadLine());
+                    int number = Convert.ToInt32(Console.ReadLine());
 
-                    Console.Write("Enter a quantity pizza: ");
-                    int quantity = Convert.ToInt32(Console.ReadLine());
-
-                    Console.Write("Enter a code for choise drink: ");
-                    int choiseDrink = Convert.ToInt32(Console.ReadLine());
-
-                    double resultPrice = 0;
-                    double discount = 0;
-                    string namePizza = string.Empty;
-
-                    if (choisePizza == (int)CodePizza.Margaritha)
+                    if (i % 2 == 0)
                     {
-                        resultPrice = quantity * (int)PricePizza.Margaritha;
-                        namePizza = "Pizza Margaritha";
-                    }
-                    else if (choisePizza == (int)CodePizza.Spicy)
-                    {
-                        resultPrice = quantity * (int)PricePizza.Spicy;
-                        namePizza = "Pizza Spicy";
-                    }
-                    else if (choisePizza == (int)CodePizza.Mexico)
-                    {
-                        resultPrice = quantity * (int)PricePizza.Mexico;
-                        namePizza = "Pizza Mexico";
-                    }
-                    else
-                    {
-                        throw new Exception("Not correctly code");
+                        sumEven += number;
+                        evenCount++;
                     }
 
-                    if (choiseDrink > 0)
+                    if (i % 2 != 0)
                     {
-                        if (choiseDrink == (int)CodeDrinks.Cola)
-                        {
-                            resultPrice += (int)PriceDrinks.Cola;
-                        }
-                        else if (choiseDrink == (int)CodeDrinks.Sprite)
-                        {
-                            resultPrice += (int)PriceDrinks.Sprite;
-                        }
-                        else if (choiseDrink == (int)CodeDrinks.Fanta)
-                        {
-                            resultPrice += (int)PriceDrinks.Fanta;
-                        }
-                        else
-                        {
-                            throw new Exception("Not correctly code");
-                        }
+                        sumOdd += number;
+                        oddCount++;
                     }
 
-                    if (resultPrice > 50)
+                    if (i % 9 == 0)
                     {
-                        discount = resultPrice / 100 * 20;
+                        sumMultipleOfNine += number;
+                        countNumberMultipleOfNine++;
                     }
-
-                    if (quantity % 5 == 0)
-                    {
-                        quantity += 1;
-                    }
-
-                    Console.WriteLine($"Name - {namePizza}, Quantity - {quantity}, Price - {resultPrice - discount}");
                 }
-                catch (Exception ex)
+
+                Console.WriteLine($"Even, sum - {sumEven}, avarage - {sumEven / evenCount}");
+                Console.WriteLine($"Odd, sum - {sumOdd}, avarage - {sumOdd / oddCount}");
+                Console.WriteLine(
+                    $"MultipleOfNine, sum - {sumMultipleOfNine}, avarage - {sumMultipleOfNine / countNumberMultipleOfNine}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            // additionally
+            // task
+            try
+            {
+                Console.Write("Enter a number: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                while (true)
                 {
-                    Console.WriteLine(ex);
+                    if (n >= 100)
+                    {
+                        break;
+                    }
+
+                    Console.Write("Enter a number: ");
+                    n = Convert.ToInt32(Console.ReadLine());
                 }
+
+                for (int i = 0; i <= n; i++)
+                    if (i % n == 0 && i % 1 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
             }
-
-            string choise = Console.ReadLine() ?? string.Empty;
-            bool showMenu = choise.ToLower() == "yes";
-
-
-            if (showMenu)
+            catch (Exception ex)
             {
-                Menu();
-            }
-            else
-            {
-                Console.WriteLine("When you are ready to order, say to me");
+                Console.WriteLine(ex.Message);
             }
         }
     }
