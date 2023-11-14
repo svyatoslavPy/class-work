0f2f81c8aacfd4c6897a5a13a4092ae46439fabd
svyatoslavPy <svyatoslavik9@gmail.com>
2023-11-14 11:52:42 +0100

    arrays

--- Program.cs
+++ Program.cs
@@ -1,54 +1,80 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ClassWork
+namespace CSharpTutorials
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // task 1
+            // TASK 1
             try
             {
-                int countNumberMultipleOfFive = 0;
-                int start = 0;
-                int end = 0;
+                int size = 6;
+                int[] salaryCompany = new int[size];
+                int sum = 0;
+                int countMonth = 6;
 
-                do
+                for (int i = 0; i < countMonth; i++)
                 {
-                    Console.Write("Enter a start: ");
-                    start = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter a salary: ");
+                    int salary = Convert.ToInt32(Console.ReadLine());
 
-                    Console.Write("Enter a end: ");
-                    end = Convert.ToInt32(Console.ReadLine());
-                } while ((start <= 0 || end <= 0));
-
-                Console.WriteLine("Number all in range: ");
-                for (int i = start; i <= end; i++)
-                {
-                    Console.WriteLine(i);
-
-                    if (i % 7 == 0)
-                    {
-                        Console.WriteLine(i);
-                    }
-
-                    if (i % 5 == 0)
-                    {
-                        countNumberMultipleOfFive++;
-                    }
+                    salaryCompany[i] = salary;
                 }
 
-                Console.WriteLine($"Count number multiple of five - {countNumberMultipleOfFive}");
-                Console.WriteLine("Number less: ");
-
-                for (int i = end; i >= start; i--)
+                foreach (var item in salaryCompany)
                 {
-                    Console.Write(i);
+                    sum += item;
                 }
-            }
-            catch (FormatException)
+
+                Console.WriteLine($"Total - {sum}");
+            } catch (Exception ex)
             {
-                Console.WriteLine("Not correct, again write number");
+                Console.WriteLine(ex.Message);
+            }
+
+           
+
+            // TASK 2
+            try
+            {
+                int size = 12;
+                int[] salaryCompany2 = new int[size];
+                var rnd = new Random();
+                int min = int.MaxValue;
+                int max = int.MinValue;
+
+                const int countMonth = 12;
+                const int rndMin = 100, rndMax = 2000;
+               
+        
+                for (int i = 0; i < countMonth; i++)
+                {
+                    salaryCompany2[i] = rnd.Next(rndMin, rndMax);
+                }
+
+                for (int i = 0; i < salaryCompany2.Length; i++)
+                {
+                    if (salaryCompany2[i] <= min)
+                    {
+                        min = salaryCompany2[i];
+                    }
+                    if (salaryCompany2[i] >= max)
+                    {
+                        max = salaryCompany2[i];
+                    }
+                }
+                Console.WriteLine("----");
+
+                Console.WriteLine($"Min - {min}");
+                Console.WriteLine($"Max - {max}");
+                Console.WriteLine("----");
+
+
             }
             catch (Exception ex)
             {
@@ -56,82 +82,31 @@ namespace ClassWork
             }
 
 
-            // task 2
+            // TASK 3
             try
             {
-                Console.WriteLine();
-                Console.WriteLine("Enter a number: ");
-
-                int evenCount = 0;
-                int oddCount = 0;
-                int countNumberMultipleOfNine = 0;
-                int sumEven = 0;
-                int sumOdd = 0;
-                int sumMultipleOfNine = 0;
+                int size = 5;
+                int[] numbers = new int[size];
+                int[] numbersUpdate = new int[numbers.Length - 1];
+                const int newValue = -1;
 
 
-                for (int i = 0; i < 2; i++)
+                for (int i = 0; i < numbers.Length - 1; i++)
                 {
-                    int number = Convert.ToInt32(Console.ReadLine());
-
-                    if (i % 2 == 0)
-                    {
-                        sumEven += number;
-                        evenCount++;
-                    }
-
-                    if (i % 2 != 0)
-                    {
-                        sumOdd += number;
-                        oddCount++;
-                    }
-
-                    if (i % 9 == 0)
-                    {
-                        sumMultipleOfNine += number;
-                        countNumberMultipleOfNine++;
-                    }
+                    numbersUpdate[i] = newValue;
                 }
 
-                Console.WriteLine($"Even, sum - {sumEven}, avarage - {sumEven / evenCount}");
-                Console.WriteLine($"Odd, sum - {sumOdd}, avarage - {sumOdd / oddCount}");
-                Console.WriteLine(
-                    $"MultipleOfNine, sum - {sumMultipleOfNine}, avarage - {sumMultipleOfNine / countNumberMultipleOfNine}");
-            }
+
+                foreach (int item in numbersUpdate)
+                {
+                    Console.WriteLine(item); // show
+                }
+            } 
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
 
-
-            // additionally
-            // task
-            try
-            {
-                Console.Write("Enter a number: ");
-                int n = Convert.ToInt32(Console.ReadLine());
-
-                while (true)
-                {
-                    if (n >= 100)
-                    {
-                        break;
-                    }
-
-                    Console.Write("Enter a number: ");
-                    n = Convert.ToInt32(Console.ReadLine());
-                }
-
-                for (int i = 0; i <= n; i++)
-                    if (i % n == 0 && i % 1 == 0)
-                    {
-                        Console.WriteLine(i);
-                    }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
         }
     }
 }
