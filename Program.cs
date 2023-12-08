4914c6e9c81b960b907208e4908d60b8ae30d124
svyatoslavPy <svyatoslavik9@gmail.com>
2023-12-08 10:15:14 +0100

    Регулярні вирази

--- Program.cs
+++ Program.cs
@@ -1,146 +1,67 @@
 ﻿
-
+using System.Text.RegularExpressions;
 namespace App
 {
-    class TodoList
+    class Regular
     {
-        static void ShowMenuTodo()
-        {
-            
-            Console.WriteLine("Welcome to Todo-list: ");
-            Console.WriteLine("---------------------");
-            Console.WriteLine("Manual todolist");
-            Console.WriteLine("Add - Please enter A");
-            Console.WriteLine("Delete - Please enter D");
-            Console.WriteLine("Edit - Please enter E");
-            Console.WriteLine("Exit in menu - Q");
-            Console.WriteLine("---------------------");
-        }
-
-        static void ShowTodolist(List<string?> todoList)
-        {
-            Console.WriteLine("---------------------");
-            Console.WriteLine("Todolist: ");
-            
-            foreach (var todo in todoList)
-            {
-                Console.WriteLine(todo);
-            }
-        }
-
-        static void Add(List<string?> todoList, string? task)
-        {
-            string? haveTodoIsTask = todoList.Find((item) => item == task);
-
-            if (haveTodoIsTask == task)
-            {
-                throw new Exception("Have this todo in todolist");
-            }
-
-            todoList.Add(task);
-        }
-
-        static void Edit(List<string?> todoList, string? task, string? newTask)
-        {
-            int idxFoundTask = todoList.FindIndex((item) => item == task);
-            if (idxFoundTask == -1)
-            {
-                throw new Exception("Not found this todo for edit");
-            }
-
-            if (task == "" || newTask == "")
-            {
-                throw new Exception("Not have task or new task for edit");
-            }
-
-            todoList[idxFoundTask] = newTask;
-        }
-
-        static void Delete(List<string?> todoList, string? task)
-        {
-            int idxFoundTask = todoList.FindIndex((item) => item == task);
-
-            if (idxFoundTask == -1)
-            {
-                throw new Exception("Not found this todo for delete");
-            }
-
-            todoList.RemoveAt(idxFoundTask);
-        }
 
         static void Main(string[] args)
         {
-            List<string?> todoList = new List<string?>(); // general list for todolist
-            bool isPressedKey = true; // pressed key
+            // task 1
+            string phone = "380957081753";
+            Regex regex = new Regex(@"^(\d{12})$");
 
-            ShowMenuTodo(); // MENU
-
-            try
+            if (regex.Match(phone).Success)
             {
-                do
-                {
-                    Console.Write("Please enter your key: ");
-                    var key = Console.ReadKey();
-
-                    if (key.Key == ConsoleKey.A) // add task
-                    {
-                        Console.WriteLine();
-                        Console.Write("Please enter your task: ");
-                        string? task = Console.ReadLine();
-
-                        bool taskNotString = task != null && task.All(char.IsNumber);
-                   
-                        if (taskNotString)
-                        {
-                            throw new Exception("This task not string");
-                        }
-
-                        Add(todoList, task);
-                    }
-                    else if (key.Key == ConsoleKey.E) // edit
-                    {
-                        Console.WriteLine();
-                        Console.Write("Please enter your task: ");
-                        string? task = Console.ReadLine();
-                        Console.Write("Please enter your new task: ");
-                        string? newTask = Console.ReadLine();
-
-                        Edit(todoList, task, newTask);
-                    }
-                    else if (key.Key == ConsoleKey.D) // delete
-                    {
-                        Console.WriteLine();
-                        Console.Write("Please enter your task: ");
-                        string? task = Console.ReadLine();
-
-                        Delete(todoList, task);
-                    }
-                    else if (key.Key == ConsoleKey.Q)
-                    {
-                        Console.WriteLine();
-                        isPressedKey = false;
-                    }
-                    else
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("Not correct key");
-                        isPressedKey = false;
-                    }
-                } while (isPressedKey);
+                Console.WriteLine("good phone");
             }
-            catch (Exception e)
+            else
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine("bad phone");
             }
+
+            Console.WriteLine("--------------");
+            // mobile phone 
+            string phone1 = "380957081753";
+            Regex regexMobilePhone = new Regex(@"^([+]?\d{12})$");
+
+            if (regexMobilePhone.Match(phone1).Success)
+            {
+                Console.WriteLine("This good mobile phone");
+            }
+            else
+            {
+                Console.WriteLine("This bad mobile phone");
+            }
+
             
-            try
+            // email
+
+            string myEmail = "svyatoslav@gmail.com";
+
+            Regex regexEmail = new Regex(@"^([a-z A-Z 0-9_]+[@][a-z]+[.]+[a-z]+$)"); //
+
+            if (regexEmail.Match(myEmail).Success)
             {
-                ShowTodolist(todoList);
+                Console.WriteLine("Good email");
             }
-            catch (Exception e)
+            else
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine("Bad email");
             }
+
+            string fullName = "Georgiyev Svyatoslav Sergeyevich";
+            Regex regexFullName = new Regex(@"^([A-Z]+[a-z]+[ ]+[A-Z]+[a-z]+[ ]+[A-Z]+[a-z]+)$");
+
+            if (regexFullName.Match(fullName).Success)
+            {
+                Console.WriteLine("Good fullname");
+            }
+            else
+            {
+                Console.WriteLine("Bad fullname");
+            }
+
         }
     }
 }
