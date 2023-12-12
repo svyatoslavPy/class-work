c30b229bb98ecc4633a40dd956609d2083b31e8d
svyatoslavPy <svyatoslavik9@gmail.com>
2023-12-12 13:46:31 +0100

    update

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace App
                    
             var city = new City("Одесса", "Одесская область", "Ukraine",  "1 010 537", 65037, 048);
             city.showInfo();
-            Console.WriteLine("-----------");
+            Console.WriteLine("--------------");
             var country = new Country("Украина", "Европа", "43,79 млн", 380, "Kyiv", cityNames);
             country.showInfo();
         }
